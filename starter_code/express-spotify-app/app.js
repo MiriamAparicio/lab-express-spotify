e12926ab5f1d5b6e77729223a115832ab5bd1353
Miriam Aparicio <miriam.aparicio@gmail.com>
2018-06-17 18:00:04 +0200

    .env api keys

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -3,8 +3,9 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const hbs = require('hbs');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const path = require('path');
+require('dotenv').config();
 
 // create app
 const app = express();
@@ -12,24 +13,19 @@ const app = express();
 // -- setup the app
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // configure middlewares (static, session, cookies, body, ...)
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+//  app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log('REQUEST:', req.method, req.path);
   next();
 });
-hbs.registerPartials(path.join(__dirname, '/views/partials'));
-
-// SPOTIFY setup
-// Remember to paste here your credentials
-const clientId = '09d4125dfb8e4fcb89ec7c78680cd7b4',
-  clientSecret = 'a76014f15bcc43299154e48b9292435e';
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret
 });
 
 // Retrieve an access token.
@@ -45,53 +41,66 @@ app.get('/', (req, res, next) => {
 });
 
 // search artist
-app.get('/artists',(req, res, next) => {
+app.get('/artists', (req, res, next) => {
   const artist = req.query.artist;
   spotifyApi.searchArtists(artist)
-  .then(result=> {
-    const data = {
-      artist: result.body.artists.items
-    };
-    // console.log(data.artist[0].images[0].url);
-    res.render('artists', data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(result => {
+      const data = {
+        artist: result.body.artists.items
+      };
+      // console.log(result.body.artists.items);
+      res.render('artists', data);
+    })
+    .catch(next);
 });
 
 // see albums from selected artist
 app.get('/albums/:artistId', (req, res, next) => {
   const artistId = req.params.artistId;
-  //console.log(artistId);
+  // console.log(artistId);
   spotifyApi.getArtistAlbums(artistId)
     .then(result => {
       const data = {
         album: result.body.items
       };
-      //console.log(data.album[0].name);
+      // console.log(data.album[0].name);
       res.render('albums', data);
     })
-    .catch(err => {
-      console.log(err);
-    });
-
+    .catch(next);
 });
 
 // Get tracks in an album
 app.get('/album/:id', (req, res, next) => {
   const albumId = req.params.id;
-  console.log(albumId);
-  spotifyApi.getAlbumTracks(albumId, /**{ limit: 5, offset: 1 }*/)
+  // console.log(albumId);
+  spotifyApi.getAlbumTracks(albumId)
     .then(result => {
       const data = {
         track: result.body.items
       };
-      res.render('tracks', data)
+      res.render('tracks', data);
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(next);
+});
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+// NOTE: requires a views/error.ejs template
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
 });
 
 app.listen(3000, () => console.log('hello, connected to 3000'));
